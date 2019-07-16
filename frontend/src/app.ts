95f36ec14b3f338ec2d596a398da66de22d6ae60
Christian <teeswizzle19@gmail.com>
2019-07-16 15:27:09 -0400

    koa and ejs templating init

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,7 +1,34 @@
-function test(text: String) {
-    return "Hello World. " + text;
-}
+import Koa from 'koa';
+import KoaRouter from 'koa-router'
+import render from 'koa-ejs';
 
-let myText = "Saito Asuka";
+const app = new Koa();
+const router = new KoaRouter();
 
-console.log(test(myText));
+render(app, {
+    root: `${__dirname}/views`,
+    layout: false,
+    viewExt: 'ejs'
+});
+
+app.use(async (ctx, next) => {
+    try {
+        console.log("Request Received")
+        await next();
+    } catch (err) {
+        ctx.status = 500;
+        console.error('Uncaught Error: ', err);
+    }
+});
+
+router.get('/', async (ctx, next) => {
+    await ctx.render('main', {
+        testAttr: 'Saito Asuka'
+    });
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000);
+console.log("App listening at port 3000");
