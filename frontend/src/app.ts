da955fde106ecbc0c53e60bb2417bb5a9f972e6e
Christian <teeswizzle19@gmail.com>
2019-07-16 18:59:55 -0400

    static serving and starting on image list page

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,9 +1,12 @@
 import Koa from 'koa';
 import KoaRouter from 'koa-router'
 import render from 'koa-ejs';
+import serve from 'koa-static'
 
 const app = new Koa();
 const router = new KoaRouter();
+
+app.use(serve(`${__dirname}/static`));
 
 render(app, {
     root: `${__dirname}/views`,
@@ -24,6 +27,22 @@ app.use(async (ctx, next) => {
 router.get('/', async (ctx, next) => {
     await ctx.render('main', {
         testAttr: 'Saito Asuka'
+    });
+});
+
+router.get('/images', async (ctx, next) => {
+    await ctx.render('images', {
+        images: [ 
+            { path: '/img/sana_1.jpg', name: 'sana', description: 'desc' },
+            { path: '/img/sana_2.jpg', name: 'sana', description: 'desc' },
+            { path: '/img/sana_3.jpg', name: 'sana', description: 'desc' },
+            { path: '/img/mina_1.jpg', name: 'mina', description: 'desc' },
+            { path: '/img/mina_2.jpg', name: 'mina', description: 'desc' },
+            { path: '/img/mina_3.jpg', name: 'mina', description: 'desc' },
+            { path: '/img/nayeon_1.jpg', name: 'nayeon', description: 'desc' },
+            { path: '/img/nayeon_2.jpg', name: 'nayeon', description: 'desc' },
+            { path: '/img/nayeon_3.jpg', name: 'nayeon', description: 'desc' },
+        ]
     });
 });
 
